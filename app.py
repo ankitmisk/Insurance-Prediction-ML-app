750f51a37ad56ac690f97177712abd0feb6c069d
Ankit Mishra <116267683+ankitmisk@users.noreply.github.com>
2026-09-15 13:11:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ for index, col_name in enumerate(X.columns):
 
   all_ans.append(value)
 
-ud = {j:all_ans[i] for i,j in enumerate(x.columns)}
+ud = {j:all_ans[i] for i,j in enumerate(X.columns)}
 user_df = pd.DataFrame(ud)
 st.write(user_df)
 
